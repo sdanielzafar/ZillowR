20905c3a28765a983c37609cdadb9a622588751f
Justin Brantley <Justin.Brantley@nordstrom.com>
2016-03-07 12:41:34 -0800

    Beef-up test; should be identical

--- tests/testthat/test-url_encode_request.R
+++ tests/testthat/test-url_encode_request.R
@@ -5,7 +5,7 @@ test_that("function creates proper URL request string", {
     character_arg <- 'Here I am!'
     numeric_arg <- 100
 
-    expect_equivalent(
+    expect_identical(
         url_encode_request('http://example.com/API.htm', absent_arg = absent_arg, character_arg = character_arg, numeric_arg = numeric_arg),
         'http://example.com/API.htm?character_arg=Here%20I%20am!&numeric_arg=100'
     )
