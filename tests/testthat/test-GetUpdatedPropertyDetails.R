b0741f364b296770157c818bb395caffda5e01c6
Justin Brantley <Justin.Brantley@nordstrom.com>
2016-03-10 12:41:45 -0800

    Use single/double quotes consistently

--- tests/testthat/test-GetUpdatedPropertyDetails.R
+++ tests/testthat/test-GetUpdatedPropertyDetails.R
@@ -6,7 +6,7 @@ test_that("'getURL' errors are handled gracefully", {
 
     with_mock(
         getURL = function(...) {stop('Cryptic getURL error')},
-        expect_error(GetUpdatedPropertyDetails(zpid = 48749425), 'Zillow API call with request ".+" failed with Error in RCurl::getURL\\(request\\): Cryptic getURL error'),
+        expect_error(GetUpdatedPropertyDetails(zpid = 48749425), "Zillow API call with request '.+' failed with Error in RCurl::getURL\\(request\\): Cryptic getURL error"),
         .env = 'RCurl'
     )
 })
